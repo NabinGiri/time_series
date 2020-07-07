fb38bb5cf4e6ce7b79a74ec5fc201487dcd5eeb4
Nabin <39278928+NabinGiri@users.noreply.github.com>
2020-07-07 00:05:55 +0000

    Add files via upload

---
+++ timeseries2.R
@@ -0,0 +1,39 @@
+library(forecast)
+library(fUnitRoots)
+
+#importing the data
+data <- woolyrnq
+
+#looking the data
+tail(data)
+
+#plotting the data to see if there is trend
+plot(data)
+
+
+#checking if the data is stationary or non stationary by ADF test
+#if p-value > 0.05, data is non-stationary else stationary.
+#if data is non-stationary, change to stationary
+adfTest(data)
+
+#changing data to stationary
+#differencing the data
+plot(diff(data))
+
+#checking again the p-value
+adfTest(diff(data))
+
+#running AR - I - MA test
+ARIMAfit <- auto.arima(log10(data), approximation = TRUE, trace=TRUE)
+summary(ARIMAfit)
+
+#predicting the future values for three years i.e 36
+pred <- predict(ARIMAfit, n.ahead = 36)
+pred
+
+
+#integral. showing the values
+10^(pred$pred)
+
+plot(forecast(ARIMAfit, h=36))
+
